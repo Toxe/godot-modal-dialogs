1e7fd837b4d64c08c1322368222a6be6bb0413d2
Toxe <toxe@toxe.net>
2023-10-31 15:34:57 +0100

    Add Notification

---
+++ T05_ModalDialogs/notification.gd
@@ -0,0 +1,13 @@
+class_name Notification extends CanvasLayer
+
+
+func _ready() -> void:
+    await get_tree().create_timer(1.0).timeout
+
+    var tween := create_tween()
+    tween.tween_property($PanelContainer, "modulate", Color.TRANSPARENT, 0.5)
+    tween.tween_callback(queue_free)
+
+
+func set_text(text: String) -> void:
+    $PanelContainer/Label.text = text
