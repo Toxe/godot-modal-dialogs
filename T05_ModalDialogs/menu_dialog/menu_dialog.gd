aef4b928107f46cbbacf4a94d7d8513a63cb8684
Toxe <toxe@toxe.net>
2023-11-08 14:37:26 +0100

    Enable joypad menu controls

--- T05_ModalDialogs/menu_dialog/menu_dialog.gd
+++ T05_ModalDialogs/menu_dialog/menu_dialog.gd
@@ -3,8 +3,8 @@ class_name MenuDialog extends MarginContainer
 signal menu_dialog_closed(close_all_dialogs: bool)
 
 
-func _unhandled_key_input(event: InputEvent) -> void:
-    if event.is_action_pressed("quit"):
+func _unhandled_input(event: InputEvent) -> void:
+    if event.is_action_pressed("ui_cancel"):
         get_viewport().set_input_as_handled()
         close_dialog(false)  # pressed escape --> only close the top menu dialog
 
